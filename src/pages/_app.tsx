8eb5c561680b882c6491883f7bb7dd9ae9b894f5
0x7s0lt1 <zsoltfehervari@gmx.com>
2024-09-29 19:48:07 +0200

    refactor: ambient

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
-import '../styles/ambient.min.css'
-
 
 // TODO:
 // - Category filter
-// - Ambient background start fix
 // - Color palette
 // - Filter by color palette
 // ```
